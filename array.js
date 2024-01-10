f2bc53f6546069397dd274b063ccda39d8ea487d
Samuelheletsi <samuelheletsi@gmail.com>
2024-01-10 15:55:29 +0000

    r3

--- array.js
+++ array.js
@@ -1,19 +1,21 @@
 
 function processArray(event) {
-            event.preventDefault();
+    
+    event.preventDefault();
 
-           
-            const numbersInput = document.getElementById('numbersInput').value;
-            const targetNumber = document.getElementById('targetNumberInput').value;
+    
+    const numbersInput = document.getElementById('numbersInput').value;
+    const targetNumber = document.getElementById('targetNumberInput').value;
 
-            
-            const numbersArray =( numbersInput.split(' ').map(Number));
+   
+    const numbersArray = numbersInput.split(' ').map(Number);
+t
+    const occurrences = numbersArray.filter(num => num === parseInt(targetNumber)).length;
 
-            
-            const occurrences = numbersArray.filter(num => num === parseInt(targetNumber)).length;
-            const positions = numbersArray.map((num, index) => num === parseInt(targetNumber) ? index + 1 : null).filter(pos => pos !== null);
+   
+    const positions = numbersArray.map((num, index) => num === parseInt(targetNumber) ? index + 1 : null).filter(pos => pos !== null);
 
-            
-            const resultText = `The number ${targetNumber} appears ${occurrences} time(s) at position(s): ${positions.join(', ')}`;
-            document.getElementById('resultText').textContent = resultText;
-        }
+    
+    const resultText = `The number ${targetNumber} appears ${occurrences} time(s) at position(s): ${positions.join(', ')}`;
+    document.getElementById('resultText').textContent = resultText;
+}
