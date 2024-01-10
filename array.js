6e6f4e733ab80c167b834323e94b9d57ab316e39
Samuelheletsi <samuelheletsi@gmail.com>
2024-01-10 15:46:01 +0000

    r2

--- array.js
+++ array.js
@@ -1,24 +1,19 @@
+
 function processArray(event) {
-    event.preventDefault();
+            event.preventDefault();
 
-    const numbersInput = document.getElementById('numbersInput').value;
-    const targetNumber = document.getElementById('targetNumberInput').value;
+           
+            const numbersInput = document.getElementById('numbersInput').value;
+            const targetNumber = document.getElementById('targetNumberInput').value;
 
-    const numbersArray = numbersInput.split(' ').map(Number);
+            
+            const numbersArray =( numbersInput.split(' ').map(Number));
 
-    const occurrences = [];
-    const positions = [];
+            
+            const occurrences = numbersArray.filter(num => num === parseInt(targetNumber)).length;
+            const positions = numbersArray.map((num, index) => num === parseInt(targetNumber) ? index + 1 : null).filter(pos => pos !== null);
 
-    numbersArray.forEach((num, index) => {
-        if (num === parseInt(targetNumber)) {
-            occurrences.push(index );
-            positions.push(num);
+            
+            const resultText = `The number ${targetNumber} appears ${occurrences} time(s) at position(s): ${positions.join(', ')}`;
+            document.getElementById('resultText').textContent = resultText;
         }
-    });
-
-    const resultText = `The number ${targetNumber} appears ${occurrences.length} time(s) at position(s): ${positions.join(', ')}`;
-    document.getElementById('resultText').textContent = resultText;
-
-    console.log("Occurrences:", occurrences);
-    console.log("Positions:", positions);
-}
